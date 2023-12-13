d955d3d0b4e1b8817fee8fbbe7a76cf66ff5c266
Abhishek Mahala <83512997+abhishekcodecrafter@users.noreply.github.com>
2023-12-13 23:07:36 +0530

    Update get_links.py

--- routes/get_links.py
+++ routes/get_links.py
@@ -1,58 +1,8 @@
 from flask import Flask, request, jsonify
 from pytube import YouTube
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 import time
 
 app = Flask(__name__)
-
-@app.route('/get_insta_download_link', methods=['GET'])
-def get_insta_download_link():
-    link_to_reel = request.args.get('link_to_reel')
-    if not link_to_reel:
-        return jsonify(error='link_to_reel parameter is required.'), 400
-
-    try:
-        download_link = fetch_insta_download_link(link_to_reel)
-        return jsonify(download_link=download_link)
-
-    except Exception as e:
-        return jsonify(error='An error occurred while processing the request.'), 500
-
-def fetch_insta_download_link(link_to_reel):
-    # Set the path to Chromedriver executable
-    PATH = "C:\\Program Files\\chromedriver\\chromedriver.exe"
-
-    # Instantiate Chrome WebDriver with options
-    chrome_options = Options()
-    # chrome_options.add_argument("--headless")  # To run the browser in headless mode (no GUI)
-    chrome_options.add_argument("--log-level=3")  
-    driver = webdriver.Chrome(PATH, options=chrome_options)
-
-    # Open the website
-    driver.get("https://instaphotodownloader.com/")
-
-    # Find the input element and enter the link
-    url_input = driver.find_element(By.ID, "url")
-    url_input.clear()
-    url_input.send_keys(link_to_reel)
-
-    # Click the download button
-    submit_button = driver.find_element(By.ID, "submit")
-    submit_button.click()
-
-    # Wait for a few seconds to let the page load the download link
-    wait_time = 5
-    time.sleep(wait_time)
-
-    # Get the download link for the reel
-    download_link = driver.find_element(By.ID, "download-btn").get_attribute("href")
-
-    # Close the browser
-    driver.quit()
-
-    return download_link
 
 @app.route('/get_youtube_download_link', methods=['GET'])
 def get_youtube_download_link():
