011a7e744f4939cd00f8da1cda4063a12f229c55
Abhishek Mahala <83512997+abhishekcodecrafter@users.noreply.github.com>
2023-12-13 22:57:28 +0530

    Update .env.js

--- .env.js
+++ .env.js
@@ -7,16 +7,11 @@ const development = {
     ...process.env,
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: '9000',
-    Meta_WA_accessToken:'EAAN29owG7ecBAN6g2npeZAOiSoc60PK4wnnqUoJrv750oOTZCnK80EJImgGRkEafZB98CahrNVRn1HXaDtvRtwJRZCjF1LiCiv0E5CqiGtP3Yg6XMD6Q0K84ugFmhXQGL5UoOsfGqGfVlL9fybPDEZAnk5yEW3gtIybCL4jw76BEuQ6CtCxI0',
-    Meta_WA_SenderPhoneNumberId: '126239180376712',
-    Meta_WA_wabaId: '108592552192465',
-    Meta_WA_VerifyToken: 'abhi213145',
+    Meta_WA_accessToken:'Your Meta Access Token',
+    Meta_WA_SenderPhoneNumberId: 'Your Bussines WA Phone Number ID From App ',
+    Meta_WA_wabaId: 'Your Bussines WA Bussiness ID From App',
+    Meta_WA_VerifyToken: 'Typein A random Security Code',
 };
-
-// Extended token 
-// EAAN29owG7ecBAN6g2npeZAOiSoc60PK4wnnqUoJrv750oOTZCnK80EJImgGRkEafZB98CahrNVRn1HXaDtvRtwJRZCjF1LiCiv0E5CqiGtP3Yg6XMD6Q0K84ugFmhXQGL5UoOsfGqGfVlL9fybPDEZAnk5yEW3gtIybCL4jw76BEuQ6CtCxI0
-
-
 
 const fallback = {
     ...process.env,
